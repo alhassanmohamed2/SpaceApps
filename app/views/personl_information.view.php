36acfeff6f2c8c4a7dd3c8037d85a3819a0cb3ec
ahmed adel <ahmedadel123422@gmail.com>
2023-10-05 14:02:56 +0300

    add files

---
+++ app/views/personl_information.view.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div style="width: 1440px; height: 1361px; position: relative; background: white">
+        <div
+            style="left: 261px; top: 430px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 31px; display: inline-flex">
+            <div style="justify-content: flex-start; align-items: center; gap: 24px; display: inline-flex">
+                <img style="width: 64px; height: 64px; border-radius: 200px; border: 2px #D4D4D8 solid"
+                    src="https://via.placeholder.com/64x64" />
+                <div
+                    style="box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.10); justify-content: flex-start; align-items: flex-start; display: flex">
+                    <div
+                        style="align-self: stretch; padding-left: 16px; padding-right: 16px; background: #FDFDFD; border-radius: 8px; overflow: hidden; border: 1px rgba(26, 26, 26, 0.10) solid; flex-direction: column; justify-content: center; align-items: center; gap: 12px; display: inline-flex">
+                        <div
+                            style="padding-top: 8px; padding-bottom: 8px; justify-content: center; align-items: center; gap: 4px; display: inline-flex">
+                            <div style="width: 16px; height: 16px; position: relative">
+                                <div
+                                    style="width: 10.67px; height: 10.67px; left: 2.67px; top: 2.67px; position: absolute; border: 2px #1A1A1A solid">
+                                </div>
+                            </div>
+                            <div
+                                style="text-align: center; color: #1A1A1A; font-size: 14px; font-family: Inter; font-weight: 600; line-height: 20px; word-wrap: break-word">
+                                Upload a profile picture</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style="justify-content: flex-start; align-items: flex-start; gap: 80px; display: inline-flex">
+                <div
+                    style="width: 419px; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 8px; display: inline-flex">
+                    <div
+                        style="width: 419px; height: 24px; justify-content: center; align-items: center; display: inline-flex">
+                        <div
+                            style="width: 419px; color: rgba(26, 26, 26, 0.50); font-size: 16px; font-family: Inter; font-weight: 500; line-height: 24px; word-wrap: break-word">
+                            Name</div>
+                    </div>
+                    <div
+                        style="align-self: stretch; height: 58px; padding-left: 16px; padding-right: 16px; padding-top: 14px; padding-bottom: 14px; background: #FDFDFD; box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.10); border-radius: 8px; border: 1px rgba(26, 26, 26, 0.20) solid; flex-direction: column; justify-content: center; align-items: flex-start; gap: 10px; display: flex">
+                        <div
+                            style="align-self: stretch; justify-content: flex-start; align-items: center; display: inline-flex">
+                            <div
+                                style="color: rgba(26, 26, 26, 0.70); font-size: 18px; font-family: Inter; font-weight: 400; line-height: 30px; word-wrap: break-word">
+                                Text</div>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    style="width: 419px; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 8px; display: inline-flex">
+                    <div
+                        style="width: 419px; height: 24px; justify-content: center; align-items: center; display: inline-flex">
+                        <div
+                            style="width: 419px; color: rgba(26, 26, 26, 0.50); font-size: 16px; font-family: Inter; font-weight: 500; line-height: 24px; word-wrap: break-word">
+                            Github Portfolio link </div>
+                    </div>
+                    <div
+                        style="align-self: stretch; height: 58px; padding-left: 16px; padding-right: 16px; padding-top: 14px; padding-bottom: 14px; background: #FDFDFD; box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.10); border-radius: 8px; border: 1px rgba(26, 26, 26, 0.20) solid; flex-direction: column; justify-content: center; align-items: flex-start; gap: 10px; display: flex">
+                        <div
+                            style="align-self: stretch; justify-content: flex-start; align-items: center; display: inline-flex">
+                            <div
+                                style="color: rgba(26, 26, 26, 0.70); font-size: 18px; font-family: Inter; font-weight: 400; line-height: 30px; word-wrap: break-word">
+                                Text</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style="justify-content: flex-start; align-items: flex-start; gap: 80px; display: inline-flex">
+                <div
+                    style="width: 419px; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 8px; display: inline-flex">
+                    <div
+                        style="width: 419px; height: 24px; justify-content: center; align-items: center; display: inline-flex">
+                        <div
+                            style="width: 419px; color: rgba(26, 26, 26, 0.50); font-size: 16px; font-family: Inter; font-weight: 500; line-height: 24px; word-wrap: break-word">
+                            Github Portfolio link </div>
+                    </div>
+                    <div
+                        style="align-self: stretch; height: 58px; padding-left: 16px; padding-right: 16px; padding-top: 14px; padding-bottom: 14px; background: #FDFDFD; box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.10); border-radius: 8px; border: 1px rgba(26, 26, 26, 0.20) solid; flex-direction: column; justify-content: center; align-items: flex-start; gap: 10px; display: flex">
+                        <div
+                            style="align-self: stretch; justify-content: flex-start; align-items: center; display: inline-flex">
+                            <div
+                                style="color: rgba(26, 26, 26, 0.70); font-size: 18px; font-family: Inter; font-weight: 400; line-height: 30px; word-wrap: break-word">
+                                Text</div>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    style="width: 419px; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 8px; display: inline-flex">
+                    <div
+                        style="width: 419px; height: 24px; justify-content: center; align-items: center; display: inline-flex">
+                        <div
+                            style="width: 419px; color: rgba(26, 26, 26, 0.50); font-size: 16px; font-family: Inter; font-weight: 500; line-height: 24px; word-wrap: break-word">
+                            Linkedin Portfolio link </div>
+                    </div>
+                    <div
+                        style="align-self: stretch; height: 58px; padding-left: 16px; padding-right: 16px; padding-top: 14px; padding-bottom: 14px; background: #FDFDFD; box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.10); border-radius: 8px; border: 1px rgba(26, 26, 26, 0.20) solid; flex-direction: column; justify-content: center; align-items: flex-start; gap: 10px; display: flex">
+                        <div
+                            style="align-self: stretch; justify-content: flex-start; align-items: center; display: inline-flex">
+                            <div
+                                style="color: rgba(26, 26, 26, 0.70); font-size: 18px; font-family: Inter; font-weight: 400; line-height: 30px; word-wrap: break-word">
+                                Text</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div
+                style="width: 918px; height: 132px; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 8px; display: flex">
+                <div
+                    style="width: 918px; height: 24px; justify-content: center; align-items: center; display: inline-flex">
+                    <div
+                        style="width: 918px; color: rgba(26, 26, 26, 0.50); font-size: 16px; font-family: Inter; font-weight: 500; line-height: 24px; word-wrap: break-word">
+                        Bio</div>
+                </div>
+                <div
+                    style="align-self: stretch; flex: 1 1 0; padding-left: 16px; padding-right: 16px; padding-top: 14px; padding-bottom: 14px; background: #FDFDFD; box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.10); border-radius: 8px; border: 1px rgba(26, 26, 26, 0.20) solid; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 10px; display: flex">
+                    <div
+                        style="align-self: stretch; justify-content: flex-start; align-items: center; display: inline-flex">
+                        <div
+                            style="color: rgba(26, 26, 26, 0.50); font-size: 14px; font-family: Inter; font-weight: 400; line-height: 20px; word-wrap: break-word">
+                            Tell the community a bit about yourself</div>
+                    </div>
+                </div>
+            </div>
+            <div style="justify-content: flex-start; align-items: flex-start; gap: 80px; display: inline-flex">
+                <div
+                    style="padding: 24px; background: white; box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05); border-radius: 8px; overflow: hidden; border: 1px rgba(26, 26, 26, 0.20) solid; justify-content: flex-start; align-items: flex-start; gap: 24px; display: flex">
+                    <div
+                        style="flex: 1 1 0; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 12px; display: inline-flex">
+                        <div
+                            style="align-self: stretch; color: #1A1A1A; font-size: 18px; font-family: Inter; font-weight: 600; line-height: 30px; word-wrap: break-word">
+                            You have a project</div>
+                        <div
+                            style="align-self: stretch; color: rgba(26, 26, 26, 0.70); font-size: 16px; font-family: Inter; font-weight: 400; line-height: 24px; word-wrap: break-word">
+                            Placeholder for accordion text.</div>
+                    </div>
+                    <div style="width: 24px; height: 24px; position: relative"></div>
+                </div>
+                <div
+                    style="height: 114px; padding: 24px; background: white; box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05); border-radius: 8px; overflow: hidden; border: 1px rgba(26, 26, 26, 0.20) solid; justify-content: flex-start; align-items: flex-start; gap: 24px; display: flex">
+                    <div
+                        style="flex: 1 1 0; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 12px; display: inline-flex">
+                        <div
+                            style="align-self: stretch; color: #1A1A1A; font-size: 18px; font-family: Inter; font-weight: 600; line-height: 30px; word-wrap: break-word">
+                            You need a project to contrbuite</div>
+                        <div
+                            style="align-self: stretch; color: rgba(26, 26, 26, 0.70); font-size: 16px; font-family: Inter; font-weight: 400; line-height: 24px; word-wrap: break-word">
+                            Placeholder for accordion text.</div>
+                    </div>
+                    <div style="width: 24px; height: 24px; position: relative"></div>
+                </div>
+            </div>
+        </div>
+        <div
+            style="left: 1060px; top: 1091px; position: absolute; justify-content: flex-start; align-items: flex-start; gap: 716px; display: inline-flex">
+            <div
+                style="box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.10); justify-content: flex-start; align-items: flex-start; display: flex">
+                <div
+                    style="align-self: stretch; padding-left: 20px; padding-right: 20px; background: #2463EB; border-radius: 8px; overflow: hidden; flex-direction: column; justify-content: center; align-items: center; gap: 12px; display: inline-flex">
+                    <div
+                        style="padding-top: 7px; padding-bottom: 7px; justify-content: center; align-items: center; gap: 12px; display: inline-flex">
+                        <div
+                            style="text-align: center; color: white; font-size: 18px; font-family: Inter; font-weight: 500; line-height: 30px; word-wrap: break-word">
+                            Continue</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div
+            style="left: 261px; top: 305px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 8px; display: inline-flex">
+            <div
+                style="text-align: center; color: black; font-size: 30px; font-family: Inter; font-weight: 500; line-height: 45px; word-wrap: break-word">
+                Personal Information</div>
+            <div
+                style="text-align: center; color: black; font-size: 20px; font-family: Inter; font-weight: 400; line-height: 32px; word-wrap: break-word">
+                Let's get to know you better!</div>
+        </div>
+        <div
+            style="left: 261px; top: 120px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 24px; display: inline-flex">
+            <div
+                style="color: black; font-size: 36px; font-family: Inter; font-weight: 500; line-height: 52px; word-wrap: break-word">
+                Let’s setup your profile</div>
+            <div style="width: 881px; height: 62px; position: relative">
+                <div
+                    style="width: 744px; left: 77px; top: 0px; position: absolute; border-radius: 8px; overflow: hidden; justify-content: flex-start; align-items: center; gap: 2px; display: inline-flex">
+                    <div style="width: 24px; height: 24px; position: relative">
+                        <div
+                            style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute; background: #2463EB; border-radius: 9999px">
+                        </div>
+                        <div style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute"></div>
+                        <div
+                            style="width: 8px; height: 8px; left: 8px; top: 8px; position: absolute; background: white; border-radius: 9999px">
+                        </div>
+                    </div>
+                    <div style="flex: 1 1 0; height: 4px; opacity: 0.30; background: #2463EB; border-radius: 100px">
+                    </div>
+                    <div style="width: 24px; height: 24px; position: relative">
+                        <div
+                            style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute; background: #D4D4D8; border-radius: 9999px">
+                        </div>
+                        <div
+                            style="width: 9.60px; height: 9.60px; left: 7.20px; top: 7.20px; position: absolute; border-radius: 9999px; border: 2px rgba(26, 26, 26, 0.30) solid">
+                        </div>
+                        <div style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute"></div>
+                    </div>
+                    <div style="flex: 1 1 0; height: 4px; opacity: 0.30; background: #2463EB; border-radius: 100px">
+                    </div>
+                    <div style="width: 24px; height: 24px; position: relative">
+                        <div
+                            style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute; background: #D4D4D8; border-radius: 9999px">
+                        </div>
+                        <div
+                            style="width: 9.60px; height: 9.60px; left: 7.20px; top: 7.20px; position: absolute; border-radius: 9999px; border: 2px rgba(26, 26, 26, 0.30) solid">
+                        </div>
+                        <div style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute"></div>
+                    </div>
+                    <div style="flex: 1 1 0; height: 4px; opacity: 0.30; background: #2463EB; border-radius: 100px">
+                    </div>
+                    <div style="width: 24px; height: 24px; position: relative">
+                        <div
+                            style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute; background: #D4D4D8; border-radius: 9999px">
+                        </div>
+                        <div
+                            style="width: 9.60px; height: 9.60px; left: 7.20px; top: 7.20px; position: absolute; border-radius: 9999px; border: 2px rgba(26, 26, 26, 0.30) solid">
+                        </div>
+                        <div style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute"></div>
+                    </div>
+                </div>
+                <div style="width: 881px; height: 30px; left: 0px; top: 32px; position: absolute">
+                    <div
+                        style="left: 273px; top: 0px; position: absolute; text-align: center; color: rgba(26, 26, 26, 0.70); font-size: 18px; font-family: Inter; font-weight: 500; line-height: 30px; word-wrap: break-word">
+                        Primary Field </div>
+                    <div
+                        style="left: 496px; top: 0px; position: absolute; text-align: center; color: rgba(26, 26, 26, 0.70); font-size: 18px; font-family: Inter; font-weight: 500; line-height: 30px; word-wrap: break-word">
+                        Select Sub-Skills</div>
+                    <div
+                        style="left: 730px; top: 0px; position: absolute; text-align: center; color: rgba(26, 26, 26, 0.70); font-size: 18px; font-family: Inter; font-weight: 500; line-height: 30px; word-wrap: break-word">
+                        Sub-Skills Details</div>
+                    <div
+                        style="left: 0px; top: 0px; position: absolute; text-align: center; color: rgba(26, 26, 26, 0.70); font-size: 18px; font-family: Inter; font-weight: 500; line-height: 30px; word-wrap: break-word">
+                        Personal Information</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
